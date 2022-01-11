7aedfbc6934dfd4a71e8a9e7e39a4a52a3b8b4f6
Si Truong <si.truongtan@gmail.com>
2022-01-11 15:46:35 +0700

    add react-bootstrap and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import logo from "./logo.svg";
+import wallet from "./wallet.svg";
 import "./App.css";
+import Button from "react-bootstrap/Button";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const createWalletHandle = () => {
+    console.log("create wallet pressed");
+  };
+  const restoreWalletHandle = () => {
+    console.log("restore wallet pressed");
+  };
+  const backupWalletHandle = () => {
+    console.log("backup wallet pressed");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>TR7 Wallet!</p>
-      </header>
+      <Router>
+        <div className="App-header">
+          <p>TR7 Wallet!</p>
+          <img src={wallet} className="App-logo" alt="wallet" />
+          <span>
+            <Button className="" onClick={createWalletHandle}>
+              <Link to="/creat-wallet">Create Wallet</Link>
+            </Button>
+            <Button onClick={restoreWalletHandle}>Restore Wallet</Button>
+          </span>
+          <Button onClick={backupWalletHandle}>Backup Wallet</Button>
+          <span>
+            <Button onClick={backupWalletHandle}>Send</Button>
+            <Button onClick={backupWalletHandle}>Receive</Button>
+          </span>
+        </div>
+      </Router>
     </div>
   );
 }
